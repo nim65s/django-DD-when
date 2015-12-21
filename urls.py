64932727e4514f2f3e4e048f18679b2031360014
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-12-21 12:48:49 +0100

    urls 1.8

--- urls.py
+++ urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 from when.views import dispo, dispos, groupes, home, ics
 
-urlpatterns = patterns('',
+app_name = 'when'
+urlpatterns = [
         url(r'^$', home, name='home'),
         url(r'^dispos$', dispos, name='dispos'),
         url(r'^dispo/(?P<moment>\d+)/(?P<dispo>\d)/$', dispo, name='dispo'),
@@ -10,4 +11,4 @@ urlpatterns = patterns('',
         url(r'^groupe/(?P<id>\d)/$', groupes, name='groupe'),
         url(r'^(?P<groupe>\d+).ics$', ics, name='ics'),
         url(r'^faq$', TemplateView.as_view(template_name='when/faq.html'), name="faq"),
-        )
+        ]
